eabb54b38e690e5203ac17e934d132a72f18f19c
Armand Aggenbag <aggenbag.a@gmail.com>
2022-09-08 14:30:49 +0200

    Uploaded Assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
+import './App.css';
 
+import { ChannelListContainer, ChannelContainer } from "./Components";
 const apiKey = 'er6gq87cht8r';
 const client = StreamChat.getInstance(apiKey);
+
 
 const app = () => {
     return <div>
         <div className="app__wrapper">
-            <Chat client={client} theme="darkMode">
+            <Chat client={client} theme="team light">
                 <ChannelListContainer
                 />
+
                 <ChannelContainer
                 />
             </Chat>
