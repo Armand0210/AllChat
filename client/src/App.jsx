33b4436468062d8224a3a29efb790051e2cca4a9
Armand Aggenbag <aggenbag.a@gmail.com>
2022-09-08 12:54:25 +0200

    First components created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = 'er6gq87cht8r';
+const client = StreamChat.getInstance(apiKey);
 
 const app = () => {
     return <div>
-        <h1>All Chat Application</h1>
+        <div className="app__wrapper">
+            <Chat client={client} theme="darkMode">
+                <ChannelListContainer
+                />
+                <ChannelContainer
+                />
+            </Chat>
+        </div>
     </div>;
 };
 
